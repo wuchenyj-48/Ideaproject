ac6783a88bfeb086d096cfea78baaa26b6d062cd
yuntao.zhou <zz__mx@yeah.net>
2019-08-01 17:31:16 +0800

    优化代码生成模板，扩展用户等

--- fortec-mscm-base/src/main/java/fortec/mscm/base/config/AppConfig.java
+++ fortec-mscm-base/src/main/java/fortec/mscm/base/config/AppConfig.java
@@ -2,11 +2,16 @@ package fortec.mscm.base.config;
 
 import fortec.common.core.annotation.EnableDefaultCoreConfig;
 import fortec.common.feign.annotation.EnableDefaultFeignConfig;
+import fortec.common.feign.clients.UserClient;
 import fortec.common.security.annotation.EnableDefaultSecurityCommonConfig;
 import fortec.common.security.annotation.EnableDefaultSecurityResourceConfig;
+import fortec.mscm.base.service.SupplierService;
+import fortec.mscm.base.userdetails.BaseUserDetailsServiceImpl;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * @Description: 程序配置信息
@@ -22,5 +27,10 @@ import org.springframework.context.annotation.Configuration;
 @EnableDefaultSecurityCommonConfig
 @EnableDefaultSecurityResourceConfig
 public class AppConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(UserClient userClient, SupplierService supplierService) {
+        return new BaseUserDetailsServiceImpl(userClient, supplierService);
+    }
 
 }
