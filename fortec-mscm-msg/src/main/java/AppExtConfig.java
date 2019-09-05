2cfe6bdf5e13fa1db8c966e603b8d8d64ab7a5f5
yuntao.zhou <zz__mx@yeah.net>
2019-09-05 09:17:49 +0800

    增加Apollo配置

---
+++ fortec-mscm-msg/src/main/java/AppExtConfig.java
@@ -0,0 +1,15 @@
+import fortec.mscm.feign.annotation.EnableMscmFeignConfig;
+import fortec.mscm.security.annotation.EnableMscmSecurityCommonConfig;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description:
+ * @Author: yuntao.zhou
+ * @CreateDate: 2019/8/14 9:21
+ * @Version: 1.0
+ */
+@Configuration
+@EnableMscmFeignConfig
+@EnableMscmSecurityCommonConfig
+public class AppExtConfig {
+}
