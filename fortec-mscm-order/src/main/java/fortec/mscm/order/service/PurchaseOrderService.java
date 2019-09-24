ef1272fc1bed28a24f7b2a2f7a69079a48c0be53
yjyang <1440876346@qq.com>
2019-09-24 11:38:24 +0800

    发货弹框

--- fortec-mscm-order/src/main/java/fortec/mscm/order/service/PurchaseOrderService.java
+++ fortec-mscm-order/src/main/java/fortec/mscm/order/service/PurchaseOrderService.java
@@ -10,11 +10,11 @@ import fortec.common.core.service.IBaseService;
 import java.util.List;
 
 /**
-* 采购订单 service 接口
-*
-* @author chenchen
-* @version 1.0
-*/
+ * 采购订单 service 接口
+ *
+ * @author chenchen
+ * @version 1.0
+ */
 public interface PurchaseOrderService extends IBaseService<PurchaseOrder> {
 
     List<PurchaseOrder> list(PurchaseOrderQueryRequest request);
@@ -24,6 +24,7 @@ public interface PurchaseOrderService extends IBaseService<PurchaseOrder> {
 
     /**
      * 添加订单
+     *
      * @param entity
      * @return
      */
@@ -31,34 +32,48 @@ public interface PurchaseOrderService extends IBaseService<PurchaseOrder> {
 
     /**
      * 采购订单-提交 将订单状态 制单0改为待审核1
+     *
      * @param id
      */
     void submitOrder(String id);
 
     /**
      * 待审核订单-通过 将订单状态 待审核1改为供应商待确认2
+     *
      * @param id
      */
     void passOrder(String id);
 
     /**
      * 供应商待确认订单
+     *
      * @param request
      * @return
      */
     IPage<PurchaseOrder> pageForSupplier(PurchaseOrderQueryRequest request);
 
     /**
+     * @param request 查询未发货和部分发货订单
+     * @Description:
+     * @return: com.baomidou.mybatisplus.core.metadata.IPage<fortec.mscm.order.entity.PurchaseOrder>
+     * @author Yangjy
+     */
+    IPage<PurchaseOrder> pageForDelivery(PurchaseOrderQueryRequest request);
+
+    /**
      * 供应商待确认订单-可供货 将供应商确认状态 待确认0改为确认可供货1
+     *
      * @param id
      */
     void able(String id);
 
     /**
      * 供应商待确认订单-可供货 将供应商确认状态 待确认0改为确认不可供货2
+     *
      * @param id
      */
     void disable(String id);
+
 
 }
     
